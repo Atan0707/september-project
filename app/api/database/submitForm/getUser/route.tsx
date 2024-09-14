d4db24c3a13e63a0be675b827a21af21e969c2ae
Hariz Hakim <harizhakim85@gmail.com>
2024-09-14 21:19:20 +0800

    fix

--- app/api/database/submitForm/getUser/route.tsx
+++ app/api/database/submitForm/getUser/route.tsx
@@ -3,6 +3,14 @@ export async function POST(req: Request) {
         const body = await req.json();
         const { value } = body;
         console.log(value);
-        return Response.json(value)
+        return new Response(JSON.stringify({ value }), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.log('Error making POST request:', error);
+        return new Response(JSON.stringify({ error: 'Failed to process request' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
     }
 }
